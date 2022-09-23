0d32205919a722f83d8465004a0b7a268395f0a6
JHughes <joseph04hughes@gmail.com>
2022-09-23 10:38:30 -0400

    Pog2

--- CSE232/lab03.cpp
+++ CSE232/lab03.cpp
@@ -3,28 +3,14 @@
 using std::cout; using std::cin; using std::endl; using std::string;
 
 string additivePersistance(int number){
-    string numAsString = std::to_string(number);
-    string returnValue;
-    int numSize = numAsString.size();
-    if(numSize == 1){
-        return "0 " + numAsString;
+    if(number > 9){
+        additivePersistance(number / 10);
     }
-    int sum = 0;
-    int counter = 0;
-    for(int i = 0; i < numSize; i++){
-        int currentDigit = 0;
-        currentDigit = (numAsString[i] - '0');
-        sum += currentDigit;
-    }
-    if(sum > 9){
-        counter++;
-        int newInput = sum;
-        returnValue = additivePersistance(newInput);
-        return returnValue;
-    }
-    returnValue = std::to_string(counter) + " " + std::to_string(sum);
-    return returnValue;
+    int currentDigit = number % 10;
+    cout << currentDigit;
+    return "";
 }
+
 int main(){
     int input;
     while(cin >> input && input >= 0){
